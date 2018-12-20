01d62a4a94255bee105effe42e9b8c4cb92a89d6
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-19 16:14:31 -0800

    Set height to 100vh for each component

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
     return (
       <div className="App">
         <Banner />
-        <SixpackCalc />
         <WorkoutProgram />
+        <SixpackCalc />
       </div>
     );
   }
