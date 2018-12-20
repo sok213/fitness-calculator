71554f9a26c8d4326e070b8175a8001ca994ef57
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-19 17:24:13 -0800

    Update banner aestetics

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,8 +5,23 @@ class Banner extends Component {
 
     render() {
         return (
-            <div className={s.bannerContainer}>
-                <p>Banner</p>
+            <div className={`
+                ${s.bannerContainer} 
+                container-fluid
+            `}>
+                <div className={`row`}>
+                    <div className={`col-lg ${s.header}`} style={{textAlign: 'center !important'}}>
+                        <div className={s.headerContent}>
+                            <h1>Are you ready to reach your aesthetic potential?</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={`row`} style={{ textAlign: 'center'}}>
+                    <div className={`col-sm ${s.getStartedCta}`}>
+                        <button type="button" class="btn btn-dark">Begin Journey</button>
+                    </div>
+                </div>
             </div>
         );
     }
