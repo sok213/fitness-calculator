e084baf331d332e9e825643b645eaef6200c698c
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-24 19:23:52 -0800

    Update header content for six pack calc

--- src/Components/SixpackCalc/SixpackCalc.js
+++ src/Components/SixpackCalc/SixpackCalc.js
@@ -5,8 +5,18 @@ class SixpackCalc extends Component {
 
     render() {
         return (
-            <div  className={s.sixPackCalcContainer}>
-                <p>Sixpack calc component.</p>
+            <div  className={`${s.sixPackCalcContainer}`}>
+                <div className={`row`}>
+                    <div className={s.headerContent}>
+                        <h3>Abdominal Pack Forteller</h3>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+                    </div>
+                </div>
+                <div className={`row ${s.calculator}`}>
+                    <div className={`col-md-12 col-xl-6`}>
+
+                    </div>
+                </div>
             </div>
         );
     }
