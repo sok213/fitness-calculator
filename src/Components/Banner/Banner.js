d0f5e8fa2ea8fd93e23abe5f6d9c946becebc1b6
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-27 22:03:18 -0800

    Add dark background for banner text on specific sized

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -21,7 +21,7 @@ class Banner extends Component {
                                 <h1>Are you ready to reach your <span className={s.aesthetic}>aesthetic</span> potential?</h1>
                                 <div className={s.underline}></div>
                             </div>
-                            <p>You have one life to live and a life should never be lived without ever experiencing your fullest potential. Your physique is a symbol of your lifestyle and who you are as a person. For some, it may take harder work than others, but generally speaking, everyone can achieve an amazing healthy physique that could drastically improve their life and well-being. Here, you will find simple guidance on how to embark on your first step to reaching your aesthetic potential.</p>
+                            <p className={s.introText}>You have one life to live and a life should never be lived without ever experiencing your fullest potential. Your physique is a symbol of your lifestyle and who you are as a person. For some, it may take harder work than others, but generally speaking, everyone can achieve an amazing healthy physique that could drastically improve their life and well-being. Here, you will find simple guidance on how to embark on your first step to reaching your aesthetic potential.</p>
                         </div>
                     </div>
                 </div>
