e670b283b106d062b1a63bd6af618851e390d6e8
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2018-12-27 22:59:53 -0800

    Set default activeSection to 1

--- src/Components/SixpackCalc/SixpackCalc.js
+++ src/Components/SixpackCalc/SixpackCalc.js
@@ -11,7 +11,7 @@ class SixpackCalc extends Component {
         super(props);
 
         this.state = {
-            activeSection: 3,
+            activeSection: 1,
             weight: null,
             bodyFatPerc: null,
             age: null,
