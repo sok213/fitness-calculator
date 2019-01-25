5ef4c59ef72e1dda7ca144513b1763cac85c1900
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2019-01-25 02:56:10 -0800

    Yarn build

--- src/App.js
+++ src/App.js
@@ -5,22 +5,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './Components/Banner/Banner.js';
 import SixpackCalc from './Components/SixpackCalc/SixpackCalc.js';
 import WorkoutProgram from './Components/WorkoutProgram/WorkoutProgram.js';
+import Navbar from './Components/Navbar/Navbar.js';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			scroll: window.pageYOffset
+			amountScrolled: window.pageYOffset
 		}
 	}
+
+	componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll, { passive: true });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        this.setState({ amountScrolled: window.pageYOffset });
+    }
 
 	render() {
 		return (
 			<div className="App">
+				{/* <Navbar /> */}
 				<Banner />
 				{/* eslint-disable-next-line */}
-				<a name="overviewTop"></a>   
 				<WorkoutProgram />
 				<SixpackCalc />
 			</div>
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import s from './Navbar.module.scss';
+import { 
+    Link,
+    Events, 
+    scrollSpy
+ } from 'react-scroll';
+
+class Navbar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            amountScrolled: window.pageYOffset
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll, { passive: true });
+        Events.scrollEvent.register('begin');
+        Events.scrollEvent.register('end');
+        scrollSpy.update();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        this.setState({ amountScrolled: window.pageYOffset });
+    }
+
+    render() {
+        return (
+            <div 
+                className={`
+                    ${s.NavbarContainer}
+                    ${this.state.amountScrolled > 120 ? s.active : s.nonActive }    
+                `} 
+            >
+                <ul className={s.mainNav}>
+                    <li>
+                        <Link 
+                            activeClass={s.navActive}
+                            to="purpose" spy={true} smooth={true} offset={-30} duration={500}
+                        >
+                            Purpose
+                            <div className={s.navUnderline}></div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            activeClass={s.navActive}
+                            to="talent" spy={true} smooth={true} offset={-30} duration={500}
+                        >
+                            Talent
+                            <div className={s.navUnderline}></div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            activeClass={s.navActive}
+                            to="impact" spy={true} smooth={true} offset={-30} duration={500}
+                        >
+                            Impact
+                            <div className={s.navUnderline}></div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            activeClass={s.navActive}
+                            to="hq" spy={true} smooth={true} offset={-30} duration={500}
+                        >
+                            HQ
+                            <div className={s.navUnderline}></div>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
